9ec02285cf28d9051d7d45a5b5b43d71060b2127
prashantyadav4646 <prashantyadav4646@gmail.com>
2024-06-01 09:09:11 +0000

    delete data

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ def get_hello(name: str):
     #return "hello "+name
     return {"name":name}
 
+@app.delete("/delete")
+def delete_data():
+    return {"status":'deleted successfully'}
+
+@app.delete("/delete/{name}/{age}")
+def delete_name(name, age):
+    return{"Status": "deleted: "+name, "age":age, "code":200}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app=app, host="0.0.0.0", port=8082)
+
+
 
     
