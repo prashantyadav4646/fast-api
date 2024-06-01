75c598d03ca2c0af928295293b65e5af54e313dc
prashantyadav4646 <prashantyadav4646@gmail.com>
2024-06-01 08:50:37 +0000

    created get api

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-// initializing
+#from fastapi import FastAPI
+import fastapi
+app = fastapi.FastAPI()
+
+@app.get('/hello/{name}')
+def get_hello(name: str):
+    #return "hello "+name
+    return {"name":name}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app=app, host="0.0.0.0", port=8082)
+
+    
